6132141e18f195131bfb7c0337760b5674dcb7a7
Sumanth R Hegde <sumanthrh99@gmail.com>
2023-11-12 19:30:32 -0800

    cleaned up essay scraper

--- 6-puzzles/paul_graham_essay_scraper.py
+++ 6-puzzles/paul_graham_essay_scraper.py
@@ -1,20 +1,24 @@
 """
-A simply essay scraper for Paul Graham's essays. Adapted from: https://github.com/Arieg419/Paul-Graham-Essay-Scraper
+A simply essay scraper for Paul Graham's essays.
 """
 from bs4 import BeautifulSoup
 
 import requests
-import sys
 import pprint
-import time
+from tqdm import tqdm
 
 save_path = "all_essays.txt"
 
-def find_nth_occurrence(text, sub, n):
-    if n == 1:
-        return text.find(sub)
-    start = text.find(sub)
-    return find_nth_occurrence(text[start + 1:], sub, n - 1)
+def get_article_text(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    # Main text is within <table> tags
+    # and the second table contains the article text.
+    if len(soup.find_all('table')) < 2:
+        print("bad layout")
+        return "" # bad layout
+    article_text = soup.find_all('table')[1].get_text()
+    return article_text
 
 
 pp = pprint.PrettyPrinter(indent=4)
@@ -25,33 +29,14 @@ articles = {}
 
 soup = BeautifulSoup(data, "html.parser")
 
-# Get List of Articles to scrape. Hacky but works
-fonts = soup.find_all('font')
-for f in fonts:
-    article_anchor_tag = f.find('a')
-    anchor_string = str(article_anchor_tag)
-    ts, te = anchor_string.find(
-        '>'), find_nth_occurrence(anchor_string, '<', 2)
-    us, ue = find_nth_occurrence(
-        anchor_string, '=', 1), find_nth_occurrence(anchor_string, '>', 1)
-    title = anchor_string[ts + 1:te + 1]
-    url = base_url + anchor_string[us + 2:ue - 1]
-    articles[title] = url
+all_articles_text = ""
 
-for k, v in articles.items():
-    r = requests.get(v)
-    data = r.text
-    soup = BeautifulSoup(data, "html.parser")
-    tables = soup.find_all('table')
-    if len(tables) < 2:  # weird layout
-        continue
-    curr_table = tables[1]
-    pp.pprint(curr_table)
-    article = open(k.replace(" ", "").replace("/", "") + ".html", "w+")
-    try:
-        article.write(str(curr_table))
-        article.close()
-    except Exception as e:
-        print("Something went wrong...")
-        print(type(e))
-        print(str(e))
+# Extract articles URLs
+for link in tqdm(soup.select('font > a')):
+    article_url = base_url + link.get('href')
+    all_articles_text += get_article_text(article_url) + "\n\n"
+
+
+# Save all articles text into one file
+with open("all_pg_essays.txt", "w") as file:
+    file.write(all_articles_text)
